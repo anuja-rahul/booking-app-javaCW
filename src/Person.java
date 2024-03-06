c4f83b0c31e3c73cddeccfdbd695826f87152629
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-06 22:38:18 +0530

    added user and ticket classes

---
+++ src/Person.java
@@ -0,0 +1,5 @@
+public class Person {
+    private String name;
+    private String surname;
+    private String email;
+}
---
+++ src/Ticket.java
@@ -0,0 +1,6 @@
+public class Ticket {
+    private char row;
+    private int seat;
+    private double price;
+    private Person person;
+}
