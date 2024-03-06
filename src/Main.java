d9b630b3fc16027dfbffb516d6eea8682108162d
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-06 21:57:43 +0530

    added menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("""
+
+                **********************************************
+                *                 MENU OPTIONS               *
+                **********************************************
+                   1) Buy a seat
+                   2) Cancel a seat
+                   3) Find first available seat
+                   4) Show seating plan
+                   5) Print tickets information and total sales
+                   6) Search ticket
+                   0) Quit
+                **********************************************
+                """);
     }
 }
