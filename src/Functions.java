312f6279080e8126b4aa084e030d9b19b441b16e
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-07 22:33:28 +0530

    added functions class

---
+++ src/Functions.java
@@ -0,0 +1,35 @@
+public class Functions {
+
+    /**
+     * Adds a given item to the current array by assigning all the elements of
+     * the current array and the new value to a new array with a greater length than
+     * the current array and returns the new array.
+
+     * @param currentArray
+     *              array that need to be updated.
+     * @param value
+     *              new value that need to be added to the given array.
+     */
+    // Since dynamics arrays are prohibited in this coursework.
+    public static String[] updateArray(String[] currentArray, String value){
+        int newLength = currentArray.length +1;
+        String[] newArray = new String[newLength];
+
+        for(int count = 0; count < currentArray.length; count++){
+            newArray[count] = currentArray[count];
+        }
+        newArray[newLength - 1] = value;
+        return newArray;
+    }
+
+    public static boolean checkArrayValues(String value, String[] array){
+        boolean result = false;
+        for(String item: array){
+            if(item.equals(value)){
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+}
