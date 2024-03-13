3f81e081f3a77487326c4b060506ddcb0bec0de9
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-13 19:15:13 +0530

    added documentation

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ import java.util.*;
  * <a href="https://github.com/anuja-rahul/booking-app-javaCW/blob/main/Documentation/index.html">Documentation</a>.
  *
  * @author Anuja Rahul Gunasinghe
- * @since @2024-03-10
+ * @since 2024-03-10
  * @version 1.0
  */
 public class Main {
@@ -158,6 +158,13 @@ public class Main {
 
     }
 
+    /**
+     * Checks the given value against a predetermined set of data.
+     * @param choice
+     *          integer value of the inputted choice.
+     * @return
+     *          boolean value depending on the availability of the choice
+     */
     private static boolean getChoice(int choice){
         int[] responses = {0, 1, 2, 3, 4, 5, 6};
         boolean present = false;
@@ -171,6 +178,12 @@ public class Main {
         return present;
     }
 
+    /**
+     * Prompts the user and requests to input their firstname, surname and email.
+     * and returns the taken values as a string array.
+     * @return
+     *          a string array of inputs
+     */
     private static String[] getUserInfo(){
         Scanner scan = new Scanner(System.in);
 
@@ -186,6 +199,12 @@ public class Main {
         return new String[]{firstName, surName, email};
     }
 
+    /**
+     * Prompts the user and requests to input their choice of seat row and column.
+     * and returns the taken values as a string array.
+     * @return
+     *          a string array of inputs
+     */
     private static String[] getSeatInfo(){
         Scanner scan = new Scanner(System.in);
 
@@ -199,6 +218,21 @@ public class Main {
         return new String[]{currentRow, column};
     }
 
+    /**
+     * Creates a new Ticket object, generate new tickets and update the ticketRecord database.
+     * @param column
+     *              seat column of user choice
+     * @param row
+     *              seat row of user choice
+     * @param person
+     *              a Person object representing the currently active user
+     * @param dataBase
+     *              a DataHandler object to keep track of every sale and ticket
+     * @param bought
+     *              if true, ticket will be considered bought and vise versa
+     * @return
+     *              A new Ticket object containing the all the seat information and a Person object
+     */
     private static Ticket manageTicket(String column, String row, Person person, DataHandler dataBase, boolean bought){
         double price = Ticket.getPrice(column);
         Ticket newTicket = new Ticket(column, (row + column), price, person);
@@ -211,6 +245,6 @@ public class Main {
 
 
 /*
-For further references, control flow diagrams and documentation visit:
-    https://github.com/anuja-rahul/booking-app-javaCW/blob/main/Documentation/index.html
+For further references and documentation visit:
+    https://github.com/anuja-rahul/booking-app-javaCW
  */
